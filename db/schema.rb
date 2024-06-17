a09a37763fdd4e0155f76ce5a9918c26760c395c
ArthurScortegagna <arthurscortegagna@gmail.com>
2024-06-17 01:34:59 -0300

    ...

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema[7.1].define(version: 2024_05_27_040450) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
 ActiveRecord::Schema[7.1].define(version: 2024_06_17_034042) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
